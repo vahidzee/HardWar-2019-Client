235a2fb7f10bc0bb78fa727f700dbd0c713f0e39
vahidzee <vahid98zee@gmail.com>
2019-04-29 23:16:43 +0430

    Serial Connection Listener Process is ready

--- Serial/connection.py
+++ Serial/connection.py
@@ -1,9 +1,12 @@
 import serial
+import multiprocessing as mp
+import datetime
 
 
 class Connection:
     # variables
     output_streams = []
+    active_listener = None
 
     def __init__(
             self,
@@ -20,6 +23,53 @@ class Connection:
             timeout=timeout
         )
 
-    def listener(self, size: int = 1):
-        while True:
-            print(self.port.read(size=size))
+    def add_output_stream(self, output_stream):
+        pass
+
+    def start_listener(
+            self,
+            size: int = 4,
+            log: bool = True
+    ):
+        """
+        creates a listener process for this connection
+        """
+
+        # checking if any active listener process exists
+        if self.active_listener:
+            self.active_listener.kill()
+
+        def bytes_to_int(bytes):
+            result = 0
+
+            for b in bytes:
+                result = result * 256 + int(b)
+
+            return result
+
+        def listener_process():
+            """
+            reads data and sends it to every output stream of this serial connection
+            """
+            while True:
+                data = self.port.read(size)
+                if data:
+                    # logging data
+                    if log:
+                        print(
+                            f'\033[32;0mSerial\033[0m {self.port.name:^30}',
+                            f'\033[32;0mTime\033[0m \033[34;0m{datetime.datetime.now().time()}\033[0m',
+                            '\033[32;0m:\033[0m',
+                            data
+                        )
+
+                    # sending data to every output stream
+                    for stream in self.output_streams:
+                        stream.send(data)
+
+        self.active_listener = mp.Process(
+            target=listener_process,
+            name='listener' + self.port.name,
+            daemon=False
+        )
+        self.active_listener.start()
